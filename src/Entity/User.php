544f26c3b5c0484c2663b3f685243194b11f1c04
rada <sandradelaporte@free.fr>
2019-02-07 18:03:41 +0100

    install FosUser

---
+++ src/Entity/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
